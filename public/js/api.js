d79082baf922074f78825e49901dda55b79fc70b
CrazyBotBoy <57533877+PasinduDushan@users.noreply.github.com>
2021-08-25 18:54:49 +0530

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+let api_key = "c71cc98b8179132b0059c14fcea045bf";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
